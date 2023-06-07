2b5be7cfb6475657ae0f741349f70f4e808843fd
talate <erylmz.talat@gmail.com>
2023-06-07 19:08:04 +0200

    Initial commit

---
+++ ItunesDemoApp/ItunesDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesDemoAppApp.swift
+//  ItunesDemoApp
+//
+//  Created by talate on 7.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ItunesDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
